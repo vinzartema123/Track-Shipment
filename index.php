0f04d729aabc2ffdcf02149a7f1bf15561eaac62
vinzartema123 <MalazarteA@voltex.local>
2022-02-24 16:38:44 +0800

    Population and Save meta
    Population and Save meta

--- index.php
+++ index.php
@@ -65,15 +65,6 @@
 			
 		if (isset($_POST['import'])) {
 
-		/* if (is_uploaded_file($_FILES['upload_csv']['tmp_name'])) {
-
-				echo "uploaded successfully.";
-		} */
-		 echo "fasdfas";
-		 print_r($_FILES);
-			// $fileContent = file_get_contents($_FILES['upload_csv']['tmp_name']);
-
-			// print_r( $fileContent);
 			//Import uploaded file to Database
 		$original = ini_get("auto_detect_line_endings");
 		ini_set("auto_detect_line_endings", true);
@@ -85,20 +76,7 @@
 		$column = array_map('nestedMetakey', (array)fgetcsv($handle));
 		print_r($column);
 		
-		
-		/* $my_post = array(
-		  'post_title'    => wp_strip_all_tags( "sample insert"),
-		  'post_content'  => "None",
-		  'post_type'=> 'wpcargo_shipment', 
-		  'post_status'   => 'publish',
-		  'post_author'   => 1,
-		   'meta_input'=>array(
-                '_payment_date'=>"game",
-            )
-		);
-		 
-		// Insert the post into the database
-		wp_insert_post( $my_post , true); */
+	
 		
 		$current_user = (array)wp_get_current_user();
 		
@@ -114,11 +92,12 @@
 				  print_r($line);
 				  echo "</pre>"; */  
 				  
-				  $title = array_search('ship-name', $column);
+				  $ship_title = array_search('ship-name', $column);
 				  
 				  $un_loc_code = array_search('un-loc-code', $column);
 				  $terminal_id = array_search('terminal-id', $column);
 				  $eta = array_search('eta', $column);
+				  $etd = array_search('etd', $column);
 				  $ship_operator_voyage_out = array_search('ship-operator-voyage-out', $column);
 				  $lloyds_id = array_search('lloyds-id', $column);
 				  $cargo_cutoff_date = array_search('cargo-cutoff-date', $column);
@@ -138,7 +117,25 @@
 				  $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyz';
 				 // Output: 54esmdr0qf
 				  $random_char = substr(str_shuffle($permitted_chars), 0, 20);
+				
+				
+					 // ETA
+					 
+					 
+					 $eta_dt = strtotime($line[$eta]);
+					 $wpcargo_departure_time_picker = date('H:i:s a',$eta_dt);
+					 $wpcargo_pickup_date_picker = date('Y-m-d',$eta_dt);
+					 
+					  // ETD
+					 $etd_dt = strtotime($line[$etd]);
+					 $wpcargo_pickup_time_picker = date('H:i:s a',$etd_dt);
+					 $wpcargo_expected_delivery_date_picker = date('Y-m-d',$etd_dt);
+					 
 
+					echo $wpcargo_departure_time_picker."<br>";
+					echo $line[$eta];
+				
+				
 				  $my_post = array(
 					  'post_title'    => wp_strip_all_tags($random_char),
 					  'post_content'  => "",
@@ -146,10 +143,14 @@
 					  'post_status'   => 'publish',
 					  'post_author'   => $current_user_id,
 					  'meta_input'=>array(
+							'wpcargo_departure_time_picker'=>$wpcargo_departure_time_picker,
+							'wpcargo_pickup_date_picker'=>$wpcargo_pickup_date_picker,
+							'wpcargo_pickup_time_picker'=>$wpcargo_pickup_time_picker,
+							'wpcargo_expected_delivery_date_picker'=>$wpcargo_expected_delivery_date_picker,
+							
 							'un_loc_code'=>$line[$un_loc_code],
 							'terminal_id'=>$line[$terminal_id],
-							'eta'=>$line[$eta],
-							'ship_name'=>$line[$title],
+							'wpcargo_shipper_name'=>$line[$ship_title],
 							'ship_operator_voyage_out'=>$line[$ship_operator_voyage_out],
 							'lloyds_id'=>$line[$lloyds_id],
 							'cargo_cutoff_date'=>$line[$cargo_cutoff_date],
@@ -168,12 +169,14 @@
 					   )
 					);
 					 $row++;
+					
 					 
-					 print_r($my_post);
+					/*  print_r($time);*/
+					 print_r($my_post); 
 					// Insert the post into the database
 					
 					
-					 $post_id = null;
+					$post_id = null;
 					if($id = post_exists(wp_strip_all_tags($random_char) )){
 						$post_id = $id;
 					}
@@ -181,44 +184,24 @@
 						$post_id =  wp_insert_post( $my_post );
 					}
 					
-					
-					/* echo $post_id."<br>";
-					echo "JCP".str_pad(12346435412307894656345, 10, '0', STR_PAD_LEFT);
-					 */
+				
 					
 					$post_update = array(
 						'ID'         => $post_id,
 						'post_title' => "JCP".str_pad($post_id, 10, '0', STR_PAD_LEFT)
 					  );
 
-					wp_update_post( $post_update ); 
+					wp_update_post( $post_update );   
 					
 					
 					   
-					//update_post_meta( $post_id, 'times', '1' );
-				  // echo $row;
+					
 				  unset($line);
 				  flush();
 				  ob_flush(); 
-		} while (($line = fgetcsv($handle)) !== false && $row < 20);
+		} while (($line = fgetcsv($handle)) !== false && $row < 1);
 		
-			/* while () {
-			} */
-			
-			
-			/*  while (($data = fgetcsv($handle, 1000, ',')) !== FALSE) {
-				 
-				  usleep(5); 
-				  
-				 echo "<pre>";
-				 print_r($data);
-				  echo "</pre>";
-				  
-				  unset($data);
-				  flush();
-				  ob_flush();
-			 
-			}   */
+		
 
 			fclose($handle);
 		} 
@@ -234,4 +217,42 @@
 		}
 
 		return preg_replace('/\s+/', '-', ltrim(strtolower($value)));
+	}
+
+
+	add_action('post_updated', 'save_shipment_metadata');
+
+	function save_shipment_metadata($ID = false, $post = false)
+	{
+		global $post;
+		echo "<pre>";
+		// print_r($_POST['post_type']);
+		// print_r($post->post_type);
+		
+		// print_r($_POST);
+		// die();
+		
+		if($post->post_type != 'wpcargo_shipment')return;
+		
+		
+		if(isset($_POST['un_loc_code']))update_post_meta($ID, 'un_loc_code', $_POST['un_loc_code']);
+		if(isset($_POST['terminal_id']))update_post_meta($ID, 'terminal_id', $_POST['terminal_id']);
+		if(isset($_POST['ship_operator_voyage_out']))update_post_meta($ID, 'ship_operator_voyage_out', $_POST['ship_operator_voyage_out']);
+		if(isset($_POST['lloyds_id']))update_post_meta($ID, 'lloyds_id', $_POST['lloyds_id']);
+		if(isset($_POST['cargo_cutoff_date']))update_post_meta($ID, 'cargo_cutoff_date', $_POST['cargo_cutoff_date']);
+		if(isset($_POST['reefer_cutoff_date']))update_post_meta($ID, 'reefer_cutoff_date', $_POST['reefer_cutoff_date']);
+		if(isset($_POST['operators']))update_post_meta($ID, 'operators', $_POST['operators']);
+		if(isset($_POST['operators_description']))update_post_meta($ID, 'operators_description', $_POST['operators_description']);
+		if(isset($_POST['ship_operator_code']))update_post_meta($ID, 'ship_operator_code', $_POST['ship_operator_code']);
+		if(isset($_POST['ship_operator_voyage_in']))update_post_meta($ID, 'ship_operator_voyage_in', $_POST['ship_operator_voyage_in']);
+		if(isset($_POST['export_receival_commencement_date']))update_post_meta($ID, 'export_receival_commencement_date', $_POST['export_receival_commencement_date']);
+		if(isset($_POST['import_availability_date']))update_post_meta($ID, 'import_availability_date', $_POST['import_availability_date']);
+		if(isset($_POST['import_storage_start_date']))update_post_meta($ID, 'import_storage_start_date', $_POST['import_storage_start_date']);
+		if(isset($_POST['vessel_type']))update_post_meta($ID, 'vessel_type', $_POST['vessel_type']);
+		if(isset($_POST['actual_arrival_date']))update_post_meta($ID, 'actual_arrival_date', $_POST['actual_arrival_date']);
+		if(isset($_POST['actual_depart_date']))update_post_meta($ID, 'actual_depart_date', $_POST['actual_depart_date']);
+		if(isset($_POST['vessel_code']))update_post_meta($ID, 'vessel_code', $_POST['vessel_code']);
+			
+		/* print_r($_POST);
+		die();  */
 	}
--- wpcargo-shipment-fields.php
+++ wpcargo-shipment-fields.php
@@ -2,102 +2,94 @@
 <?php 
 	//echo $id;
 	
-	print_r( get_post_meta( $id, '_payment_date' ,true));
+	// print_r( get_post_meta( $id, 'wpcargo_departure_time_picker' ,true));
 ?>
-<table class="wpcargo form-table">
+<table class="wpcargo form-table third-table">
 				<tbody>
 				
-		<tr>
+		<tr style="
+			width: 50%!important;
+			display: inline-block;
+		">
 			<th><label>UN Loc Code</label></th>
-			<td><input type="text" id="wpcargo_weight" name="wpcargo_weight" value="1" size="25"></td>
+			<td style="
+    width: 70%;
+"><input type="text" id="un_loc_code" name="un_loc_code" value="<?=get_post_meta( $id, 'un_loc_code' ,true)?>" size="25"></td>
 		</tr>
 		<tr>
 			<th><label>Terminal ID</label></th>
-			<td><input type="text" id="wpcargo_courier" name="wpcargo_courier" value="JPC" size="25"></td>
-		</tr>
-		<tr>
-			<th><label>ETA</label></th>
-			<td><input type="text" id="pack" name="wpcargo_packages" value="1" size="25"></td>
-		</tr>
-		
-		<tr>
-			<th><label>ETD</label></th>
-			<td><input type="text" id="prod" name="wpcargo_product" value="goods" size="25"></td>
-		</tr>
-		<tr>
-			<th><label>Ship Name</label></th>
-			<td><input type="text" id="wpcargo_qty" name="wpcargo_qty" value="1" size="25"></td>
-		</tr>
+			<td><input type="text" id="terminal_id" name="terminal_id" value="<?= get_post_meta( $id, 'terminal_id' ,true)?>" size="25"></td>
+		</tr>		
 		<tr>
 			<th><label>Ship Operator Voyage Out</label></th>
-			<td><input type="text" id="wpcargo_total_freight" name="wpcargo_total_freight" value="" size="25"></td>
+			<td><input type="text" id="ship_operator_voyage_out" name="ship_operator_voyage_out" value="<?= get_post_meta( $id, 'ship_operator_voyage_out' ,true)?>" size="25"></td>
 		</tr>
 		
 		<tr>
 			<th><label>Lloyds ID</label></th>
-			<td><input type="text" id="wpcargo_carrier_ref_number" name="wpcargo_carrier_ref_number" value="" size="25"></td>
+			<td><input type="text" id="lloyds_id" name="lloyds_id" value="<?= get_post_meta( $id, 'lloyds_id' ,true)?>" size="25"></td>
 		</tr>
 		<tr>
 			<th><label>Cargo Cutoff Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="cargo_cutoff_date" name="cargo_cutoff_date"  value="<?= get_post_meta( $id, 'cargo_cutoff_date' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Reefer Cutoff Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="reefer_cutoff_date" name="reefer_cutoff_date"  value="<?= get_post_meta( $id, 'reefer_cutoff_date' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Operators</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="operators" id="operators" name="operators"  value="<?= get_post_meta( $id, 'operators' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Operators Description</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="operators_description" id="operators_description" name="operators_description"  value="<?= get_post_meta( $id, 'operators_description' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Ship Operator Code</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="ship_operator_code" id="ship_operator_code" name="ship_operator_code"  value="<?=get_post_meta( $id, 'ship_operator_code' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Ship Operator Voyage In</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="ship_operator_voyage_in" id="ship_operator_voyage_in" name="ship_operator_voyage_in"  value="<?= get_post_meta( $id, 'ship_operator_voyage_in' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Export Receival Commencement Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="export_receival_commencement_date" name="export_receival_commencement_date"  value="<?=get_post_meta( $id, 'export_receival_commencement_date' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Import Availability Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="import_availability_date" name="import_availability_date"  value="<?= get_post_meta( $id, 'import_availability_date' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Import Storage Start Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="import_storage_start_date" name="import_storage_start_date"  value="<?= get_post_meta( $id, 'import_storage_start_date' ,true)?>">
 			</td>
 		</tr>
 		
@@ -105,28 +97,28 @@
 			<th><label>Vessel Type</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="vessel_type" id="vessel_type" name="vessel_type"  value="<?= get_post_meta( $id, 'vessel_type' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Actual Arrival Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="actual_arrival_date" name="actual_arrival_date"  value="<?= get_post_meta( $id, 'actual_arrival_date' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Actual Depart Date</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="wpcargo-timepicker" id="actual_depart_date" name="actual_depart_date" value="<?= get_post_meta( $id, 'actual_depart_date' ,true)?>">
 			</td>
 		</tr>
 		<tr>
 			<th><label>Vessel Code</label></th>
 			<td>
 				<label for="UpdateDate"></label>
-				<input type="text" class="wpcargo-timepicker" id="datetimepicker5" name="wpcargo_departure_time_picker" autocomplete="off" value="">
+				<input type="text" class="vessel_code" id="vessel_code" name="vessel_code"  value="<?= get_post_meta( $id, 'vessel_code' ,true)?>">
 			</td>
 		</tr>
 		</tbody></table>
